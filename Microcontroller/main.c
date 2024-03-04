51f7e1ab9e68ada90ae824cf19be02220201fee6
morten <mobin16@student.sdu.dk>
2024-03-04 12:59:32 +0100

    test

---
+++ Microcontroller/main.c
@@ -0,0 +1,35 @@
+#ifndef F_CPU
+	#define F_CPU 16000000 // clock speed is 16MHz
+#endif
+#define __AVR_atmega644p__
+#define PB1 PORTB
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+	DDRB = 0xFF; // declared port D as output
+	
+	while (1) { // initialize infinite while loop
+		//PORTB = 0b1111101; // turn on all LEDs of PORTD
+		PORTB = 0b0000010; // turn on all LEDs of PORTD
+		//PORTB = 0x04 // 0x04 = 0b00000100; // turn on all LEDs of PORTD
+		_delay_ms(1000); // delay of one second
+
+		PORTB = 0b0000000; // turn off all LEDs of PORTD
+		_delay_ms(1000); // delay of one second
+	} // while loop end
+} // main end
+
+
+
+/*
+
+0b 0000 0000      ingen
+0b 0000 0010      2
+0b 0000 0011      2 og 1
+0b 0000 0100	  3
+
+
+
+*/
---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <ur_rtde/rtde_control_interface.h>
+#include <thread>
+#include <chrono>
+#include <vector>
+
+using namespace ur_rtde;
+using namespace std::chrono; 
+
+int main(int argc, char* argv[])
+{
+	// The constructor simply takes the IP address of the Robot
+	RTDEControlInterface rtde_control("127.0.0.1");
+	// First argument is the pose 6d vector followed by speed and acceleration
+	rtde_control.moveL({-0.143, -0.435, 0.20, -0.001, 3.12, 0.04}, 0.5, 0.2);
+
+  return 0;
+}
+
+/*
+
+Cpp:
+
+Boost ( /beast(?) )
+
+
+	Interface/GUI:
+		-Terminal.. ig?
+
+
+	Classes:
+		-ur_rtde ( MEN DER ER ALLEREDE CLASS API )
+			-Connect
+			-Disconnect
+			-Status ( threading ) (isConnected(), isProgramRunning(), getRobotStatus(), isPoseWithinSafetyLimits(),  )
+			-Settings ( setTCP, )
+			---- force control ----
+			-Feature ( manuelt på robot? )			
+			-Movement ( force control )
+			- TBC...
+			
+			
+			
+			
+			
+			
+		-Microcontroller communication (ATMega644PU ( nok bare ATMega )
+			-GripperControl
+			
+		-webinterface? Næppe..	
+		-SQL ( eventlogger )
+		-Controller
+
+
+Microcontroller:
+
+	Communication:
+		-FreeMODBUS(?) https://www.embedded-experts.at/en/freemodbus/ports-ascii-rtu/avr-atmega8-16-32-128-168-169/
+		-https://github.com/dipakgmx/ATMega_TWI
+		
+	
+
+
+
+*/

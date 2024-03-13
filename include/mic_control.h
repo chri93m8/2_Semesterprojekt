2af8fae509f9c64be2766579a4b808f035ae3b44
chri93m8 <chrch23@student.sdu.dk>
2024-03-13 11:27:06 +0100

    ny void

--- include/mic_control.h
+++ include/mic_control.h
@@ -6,13 +6,17 @@ class Mic_Control{
 private:
 	
 public:
-	void Open(); 
-	//afhænger af mekanik
-	void Close();
-	//afhænger af mekanik
-	void readPotmeter();
-	//aner ikke hvordan det kommer til at virke
+
+	void requestData();
+	// spøg MC om data fra Potmeter og motor amp
 	
+	void Open(); 
+	//Send signal til MC der åbner gribbern
+	//brug requestdata() til at se hvornår den er åben (potmeter)
+	
+	void Close();
+	//Send signal til MC der åbner gribbern
+	//brug requestdata() til at se hvornår den er lukket (Motor amp) 
 	
 
 };

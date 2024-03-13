84bcc63ca5d3acee2b482e5ad62bbafee894b040
hestefyren <mobin16@student.sdu.dk>
2024-03-13 13:14:17 +0100

    main er kæmpe rod. beklager

--- include/robot_control.h
+++ include/robot_control.h
@@ -2,17 +2,22 @@
 #define ROBOT_CONTROL_H
 
 #include <ur_rtde/rtde_control_interface.h>
+#include <ur_rtde/rtde_receive_interface.h>
 #include <ur_rtde/dashboard_client.h>
 #include <iostream>
+#include <vector>
 
 class Robot_control {
 	private:
 		std::string _ip;
+		std::vector<double> _point;
+		
 	public:
 		Robot_control(std::string ip);
 		// Practical stuff
 		void connect();
 		std::string getIp();
+		std::vector<double> getPose();
 
 
 		// Movement
--- src/robot_control.cpp
+++ src/robot_control.cpp
@@ -3,23 +3,44 @@
 
 Robot_control::Robot_control(std::string ip)  {
 	_ip = ip;
+
 }
 
 void Robot_control::connect() {
 	std::cout << getIp() << std::endl;
 	
 	try {
-		ur_rtde::RTDEControlInterface rtde_control(getIp());
+		
+		//ur_rtde::RTDEControlInterface ur = ur_rtde::RTDEControlInterface rtde_control(getIp());
+		//ur_rtde::RTDEReceiveInterface rtde_receive(getIp());
+		//std::vector<double> a = rtde_receive.getActualTCPPose();
+		
+		//_point = a;
+		
 	// mutexes
 	}
 	catch (const std::exception& e) {
 		std::cout << e.what() << std::endl;
 	}
+	
 }
 
 std::string Robot_control::getIp() {
 	return _ip;
 }
+
+
+std::vector<double> Robot_control::getPose(){
+/*
+	ur_rtde::RTDEControlInterface rtde_control(getIp());
+	ur_rtde::RTDEReceiveInterface rtde_receive(getIp());
+	std::vector<double> a = rtde_receive.getActualTCPPose();
+		
+	_point = a;
+	*/
+	return _point;
+}
+
 
 
 

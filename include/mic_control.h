95a044d41154f6b7118e6762ce9249adc42f68b4
Christian Lyngholm Christiansen <chrch23@student.sdu.dk>
2024-03-21 08:27:59 +0100

    2024.03.21

--- include/mic_control.h
+++ include/mic_control.h
@@ -5,24 +5,26 @@
 
 class Mic_Control{
 private:
-	
-public:
 
 	void requestData();
-	// spøg MC om data fra Potmeter og motor amp
+	// spøg MC om data fra Potmeter og motor amp 
+	
+public:
 	
 	void Open(); 
 	//Send signal til MC der åbner gribbern
 	//brug requestdata() til at se hvornår den er åben (potmeter)
+	//returen 1 when done
 	
 	void Close();
-	//Send signal til MC der åbner gribbern
-	//brug requestdata() til at se hvornår den er lukket (Motor amp) 
+	//Send signal til MC der lukker gribbern
+	//brug requestdata() til at se hvornår den er lukket (Motor amp)
+	//returen 1 when done 
 	
-	void isGripping();
-	//istedet for request data???
-	
-
+	void gripperData();
+	//Behandeler data fra requestData() og sender det vider i programmet
+	//Returne et aray med gripperSate(0=clost 1=open) Returne diskSize(1-5)
+	 
 };
 
 #endif //MIC_CONTROL_H
